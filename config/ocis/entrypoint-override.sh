84e870c7334eabc05bcff5947be2cc21c784aa15
Willy Kloucek <wkloucek@owncloud.com>
2021-10-13 17:32:29 +0200

    switch to app provider

--- config/ocis/entrypoint-override.sh
+++ config/ocis/entrypoint-override.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-mkdir -p /var/tmp/ocis/.config/
-cp /config/proxy-config.json /var/tmp/ocis/.config/proxy-config.json
-cp /config/web-config.dist.json /var/tmp/ocis/.config/web-config.json
-sed -i 's/ocis.owncloud.test/'${OCIS_DOMAIN:-ocis.owncloud.test}'/g' /var/tmp/ocis/.config/web-config.json
-
 ocis server&
 sleep 10
 
@@ -38,9 +33,5 @@ ocis accounts remove f7fbf8c8-139b-4376-b307-cf0a8c2d0d9c
 set -e
 
 echo "##################################################"
-
-ocis kill proxy
-sleep 10
-ocis proxy server # workaround for loading proxy configuration
 
 wait # wait for oCIS to exit
